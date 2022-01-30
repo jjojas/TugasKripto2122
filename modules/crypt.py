e305b747e4349fef7914a01f7d4b499e0b3d1813
jjojas <justinjojas@gmail.com>
2022-01-30 10:02:44 +0700

    Add extended vigenere but extended ascii output error :(

--- modules/crypt.py
+++ modules/crypt.py
@@ -26,7 +26,42 @@ def vigenere_decrypt(plaintext,key):
             pass
     return cipher
 
-a = vigenere_encrypt("AYAM KALASAN ENAK SEKALI SOBAT","LEMON")
-print(a)
-b = vigenere_decrypt(a,"LEMON")
-print(b)
+def extended_vigenere_encrypt(plaintext,key):
+    cipher = ""
+    plaintext = plaintext.upper().replace(" ", "")
+    key = key.upper().replace(" ", "")
+    for i in range(len(plaintext)):
+        if ord(plaintext[i]) >=0 and ord(plaintext[i]) <=255:
+            inChar = ord(plaintext[i])
+            inKey = ord(key[i % len(key)])
+            newChar = (inChar + inKey) % 256
+            cipher +=chr(newChar)
+            # print(f"{inChar} - {newChar}")
+            # print(f"{chr(inChar)} - {chr(newChar)}")
+        else:
+            pass
+    return cipher
+
+def extended_vigenere_decrypt(plaintext,key):
+    cipher = ""
+    plaintext = plaintext.upper().replace(" ", "")
+    key = key.upper().replace(" ", "")
+    for i in range(len(plaintext)):
+        if ord(plaintext[i]) >=0 and ord(plaintext[i]) <=255:
+            inChar = ord(plaintext[i])
+            inKey = ord(key[i % len(key)])
+            newChar = (inChar - inKey) % 256
+            cipher +=chr(newChar)
+        else:
+            pass
+    return cipher
+
+if __name__ == "__main__":
+    plaintext = "4ku suka mak@n ayam, d1a suka makan somay!"
+    key = "LEMON"
+    vigenereCipher = vigenere_encrypt(plaintext,key)
+    vigenereDecipher = vigenere_decrypt(vigenereCipher,key)
+    vigenereExCipher = extended_vigenere_encrypt(plaintext,key)
+    vigenereExDecipher = extended_vigenere_decrypt(vigenereExCipher,key)
+    print(f"VC: {vigenereCipher}\nVD: {vigenereDecipher}")
+    print(f"EVC: {repr(vigenereExCipher)}\nDVC: {vigenereDecipher}")

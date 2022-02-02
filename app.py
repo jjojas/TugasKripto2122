8cd05d6762e4eee988d8fdf496b4d0d470636b70
jjojas <justinjojas@gmail.com>
2022-02-02 14:29:36 +0700

    add playfair widget to tabs

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from PyQt5.QtGui import *
 
 import interfaces.vigenere as v
 import interfaces.extendedVigenere as ev
+import interfaces.fairplayCipher as p
 
 class mainWidget(qtw.QWidget):
     def __init__(self, parent):
@@ -18,7 +19,7 @@ class mainWidget(qtw.QWidget):
         self.tabs.setFont(QFont('Roboto', 14))
         self.tabs.addTab(v.vigenereWidget(self),"Vigenere")
         self.tabs.addTab(ev.extendedVigenereWidget(self),"Extended Vigenere")
-        self.tabs.addTab(qtw.QWidget(),"Playfair")
+        self.tabs.addTab(p.playfairWidget(self),"Playfair")
         self.tabs.addTab(qtw.QWidget(),"Enigma")
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)    
